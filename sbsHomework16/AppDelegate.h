6b5d3158416a4d208d46078004f862f3b174e75c
Igor Chernyshov <ichernyshov@bk.ru>
2019-06-23 09:36:28 +0300

    Initial Commit

---
+++ sbsHomework16/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sbsHomework16
+//
+//  Created by Igor Chernyshov on 23/06/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
